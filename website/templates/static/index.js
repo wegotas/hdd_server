4e214da046c99de8e8bbd69b83d656e3c568f02a
sopenaclient <wegotas@gmail.com>
2018-06-19 17:50:07 +0300

    Changes: * Website's some visual changes.

--- website/templates/static/index.js
+++ website/templates/static/index.js
@@ -1,3 +1,8 @@
 function lot_content(index) {
 	var contentWindow = window.open('content/'+index+'/', "", "width=400,height=650");
 }
+
+function filterTest() {
+	filterDiv = document.getElementById('af-Model');
+    filterDiv.classList.toggle("show");
+}
